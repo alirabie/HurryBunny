af6ba2df1dad336f81c01bcd3b8b16dfc9ba7316
Eng Ali <alirabie>
2018-11-19 18:51:23 +0200

    ui

---
+++ platforms/android/app/build/intermediates/assets/debug/www/build/8.js
@@ -0,0 +1,42 @@
+webpackJsonp([8],{
+
+/***/ 413:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PickupServicePageModule", function() { return PickupServicePageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__pickup_service__ = __webpack_require__(273);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var PickupServicePageModule = /** @class */ (function () {
+    function PickupServicePageModule() {
+    }
+    PickupServicePageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__pickup_service__["a" /* PickupServicePage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["l" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__pickup_service__["a" /* PickupServicePage */]),
+            ],
+        })
+    ], PickupServicePageModule);
+    return PickupServicePageModule;
+}());
+
+//# sourceMappingURL=pickup-service.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=8.js.map
---
+++ platforms/android/app/build/intermediates/assets/release/www/build/0.js
@@ -0,0 +1,11 @@
+webpackJsonp([0],{
+
+/***/ 401:
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
